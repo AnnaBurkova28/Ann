9ef3955d6b018f60e3cbedc57fc8c905197966d1
AnnaBurkova28 <annaburkova28@gmail.com>
2023-03-02 15:52:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int value = 500;
+        int mile = value / 20;
+        System.out.println("NumberMiles:" + mile);
+    }
+
+
+}
